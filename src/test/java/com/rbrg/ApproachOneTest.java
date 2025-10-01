e68735f3756a537ec386ec15bc4d95de7bbb9778
ooandreoo <andre_av_5@outlook.com>
2025-10-01 09:48:20 -0300

    testing the enforcing of criteria

--- src/test/java/com/rbrg/ApproachOneTest.java
+++ src/test/java/com/rbrg/ApproachOneTest.java
@@ -2,9 +2,14 @@ package com.rbrg;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class ApproachOneTest {
-    @Test
-    public void givenApproachOneInstantiationWhenThereAreNoParametersProvidedThenExceptionIsThrown(){
-        ApproachOne approachOne = new ApproachOne();
+    @Test(expected = RuntimeException.class)
+    public void givenApproachOneClassWhenTryingToInstantiateWithParametersThatDontFollowTheCriteriaThenAnExceptionIsThrown(){
+        ArrayList<String> values = new ArrayList<>();
+        values.add("1");
+        values.add("2");
+        ApproachOne approachOne = new ApproachOne(values,1,2);
     }
 }
